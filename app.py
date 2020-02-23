4b659e3c7ed1bb26cce48b2a51ebdc81a0e3af6a
Sharique Shaikh <sharique.shaikh.0123@gmail.com>
2020-02-24 05:24:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,61 @@
+'''
+(weatherAppEnv)
+flask 1.1.1
+flask-sqlalchemy 2.4.1
+requests 2.23.0
+pytemperature 1.0
+weather map API
+
+sqlite3 file.db
+db.create_all()
+'''
+import requests
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy 
+import pytemperature
+import math
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+
+db = SQLAlchemy(app)
+
+class City(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        new_city = request.form.get('city')
+        
+        if new_city:
+            new_city_obj = City(name=new_city)
+
+            db.session.add(new_city_obj)
+            db.session.commit()
+
+    cities = City.query.all()
+
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
+
+    weather_data = []
+
+    for city in cities:
+
+        r = requests.get(url.format(city.name)).json()
+        t=pytemperature.f2c(r['main']['temp'])
+        temp=round(t)
+
+        weather = {
+            'city' : city.name,
+            'temperature' : temp,
+            'description' : r['weather'][0]['description'],
+            'icon' : r['weather'][0]['icon'],
+        }
+
+        weather_data.append(weather)
+
+
+    return render_template('weather.html', weather_data=weather_data)
